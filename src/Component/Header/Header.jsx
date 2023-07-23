ac4b25d04ad1003ccc2d589e750a1001da6e0b66
FarhanaIslamSaima <farhanachowdhury2152001@gmail.com>
2023-07-23 22:16:46 +0600

    updated

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,11 +1,69 @@
 import React from 'react';
-import {AppBar,Toolbar} from '@material-ui/core'
+import {AppBar,Toolbar,makeStyles,Button,Box,Typography, Grid} from '@material-ui/core'
+const useStyle=makeStyles({
+   header:{
+    background:'black'
+   },
+   container:{
+    display:'flex',
+    width:'100%'
+    
+   },
+   leftContainer:{
+    width:'50%'
+   },
+   rightContainer:{
+    width:"50%",
+    marginLeft:'auto',
+    justifyContent:'center',
+    alignItems:'center',
+    display:'flex',
+    '&>*':{
+       // margin:'10px'
+    margin:'5px',
+    fontWeight:'bold',
+    fontSize:'18px'
+    }
+   }
+
+})
 
 const Header = () => {
+    const classes=useStyle();
+
     return (
-       <AppBar>
+       <AppBar className={classes.header}>
         <Toolbar>
+            <Box className={classes.container}>
+            <Box className={classes.leftContainer}>
+        
+                
+
+</Box>
+<Box className={classes.rightContainer}>
+    
+       <Typography>Query</Typography>
+        <Typography>Contribute</Typography>
+       <Typography>Logout</Typography>
+   
+    
+     
+        
+        
+
+        
+     
+
+    
+
             
+          
+
+</Box>
+
+            </Box>
+            
+
         </Toolbar>
        </AppBar>
     );

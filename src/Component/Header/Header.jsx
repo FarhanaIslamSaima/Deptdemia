cf994f262c98759223e64553a4b04ea111ffd743
FarhanaIslamSaima <farhanachowdhury2152001@gmail.com>
2023-07-23 20:41:38 +0600

    updated

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {AppBar,Toolbar} from '@material-ui/core'
+
+const Header = () => {
+    return (
+       <AppBar>
+        <Toolbar>
+            
+        </Toolbar>
+       </AppBar>
+    );
+};
+
+export default Header;

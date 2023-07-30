e5c4bc0d19720cc097aa0804970c9e3e6c02df4a
Jifat <jifat@Buggy.buggy.com>
2023-07-30 09:18:20 +0600

    took name to infinity

--- src/Component/Home/Right/RightSidebar.jsx
+++ src/Component/Home/Right/RightSidebar.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Newsfeed from '../../NewsFeed/Newsfeed';
+import { getAuth, onAuthStateChanged } from '@firebase/auth';
+
 
 const RightSidebar = () => {
+    const auth = getAuth();
+    const [name, setName] = useState('') 
+    onAuthStateChanged(auth, (user)=>{
+        if(user){
+            setName(user.name);
+        }else{
+            console.log("no user")
+        }
+    })
+
+    
     return (
         <>
-        <Newsfeed/>
-        
+       <p>{name}</p>
+        <Newsfeed/>        
         </>
     );
 };
